cae32baee9024e4081e68e4c62fd5eff47ff5778
borared <boraofficial76@gmail.com>
2026-02-12 23:54:11 +0700

    Create folder main in components and create folder page in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Navbar from "./components/Navbar.jsx";
+import Main from "./Page/Main.jsx";
 
 export default function App() {
-	return (
-		<Navbar />
-	);
+    return (
+        <>
+            <Navbar />
+            <Main />
+            <section className="min-h-screen" />
+        </>
+    );
 };
