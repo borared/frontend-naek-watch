d460a8656c30e58340978a24447d790b872e9917
borared <boraofficial76@gmail.com>
2026-02-13 14:51:06 +0700

    pull from branch Bora

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,4 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Signup from "./signup/Signup.jsx";
-import Main from "./Page/Main.jsx";
-import Footer from "./components/Footer.jsx";
-
-export default function App() {
-	return (
-		<>
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/signup" element={<Signup />} />
-				</Routes>
-			</Router>
-			{/* <Signup /> */}
-			{/* <section className="min-h-screen" /> */}
-            <Footer />
-		</>
-	);
 }
