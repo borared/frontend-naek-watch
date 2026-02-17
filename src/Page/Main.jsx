2735be81c09f250a1b4841c3edb070b9a8b16b41
borared <boraofficial76@gmail.com>
2026-02-17 15:05:59 +0700

    I created MovieSlider.jsx to include all of category  (Top,Trending,Upcoming

--- src/Page/Main.jsx
+++ src/Page/Main.jsx
@@ -4,14 +4,15 @@ import GenreSelector from '../components/main/GenreSelector'
 import MovieSliderTrend from '../components/main/MovieSliderTrend'
 import MovieSliderTop from '../components/main/MovieSliderTop'
 import MovieSliderUpcomming from '../components/main/MovieSliderUpcomming'
+import MovieSlider from "../components/main/MovieSlider";
 const Main = () => {
   return (
     <div>
       <HeroSection/>
       <GenreSelector/>
-      <MovieSliderTrend />
-      <MovieSliderTop />
-      <MovieSliderUpcomming />
+      <MovieSlider category="top" title="រឿងល្បីៗ" />
+      <MovieSlider category="trending" title="កំពុងពេញនិយម" />
+      <MovieSlider category="upcoming" title="ឆាប់ៗនេះ" />
     </div>
   )
 }
