25a90fdb75db634943e1cb8a254fa23b90392926
borared <boraofficial76@gmail.com>
2026-03-30 10:23:22 +0700

    Updated imported SeriesPage

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,35 @@ import Tasks from "./Page/Tasks.jsx";
 import RedeemCheckout from "./Page/RedeemCheckout.jsx";
 import { UserProvider } from "./context/UserContext";
 import MoviesPage from "./Page/MoviePage.jsx";
-import SeriesPage from "./Page/SeriesPage.jsx"
+import SeriesPage from "./Page/SeriesPage.jsx";
 import ContactUs from "./Page/Contact.jsx";
 import AboutUs from "./Page/AboutUs.jsx";
-import SeriesPage from "./Page/SeriesPage.jsx";
 import GenrePage from "./Page/GenrePage.jsx";
 
 export default function App() {
-    return (
-        <UserProvider>
-            <BrowserRouter>
-                <Navbar />
+	return (
+		<UserProvider>
+			<BrowserRouter>
+				<Navbar />
 
-                <main className="pt-15 min-h-screen bg-black">
-                    <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/movie/:id" element={<MovieDetail />} />
-                        <Route path="/movies" element={<MoviesPage />} />
-                        <Route path="/series" element={<SeriesPage />} />
-                        <Route path="/genre/:genreName" element={<GenrePage />} />
-                        <Route path="/store" element={<Store />} />
-                        <Route path="/missions" element={<Tasks />} />
-                        <Route path="/redeem-checkout" element={<RedeemCheckout />} />
-                        <Route path="/contact" element={<ContactUs />} />
-                        <Route path="/about" element={<AboutUs />} />
-                    </Routes>
-                </main>
+				<main className="pt-15 min-h-screen bg-black">
+					<Routes>
+						<Route path="/" element={<Main />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/movie/:id" element={<MovieDetail />} />
+						<Route path="/movies" element={<MoviesPage />} />
+						<Route path="/series" element={<SeriesPage />} />
+						<Route path="/genre/:genreName" element={<GenrePage />} />
+						<Route path="/store" element={<Store />} />
+						<Route path="/missions" element={<Tasks />} />
+						<Route path="/redeem-checkout" element={<RedeemCheckout />} />
+						<Route path="/contact" element={<ContactUs />} />
+						<Route path="/about" element={<AboutUs />} />
+					</Routes>
+				</main>
 
-                <Footer />
-            </BrowserRouter>
-        </UserProvider>
-    );
+				<Footer />
+			</BrowserRouter>
+		</UserProvider>
+	);
 }
